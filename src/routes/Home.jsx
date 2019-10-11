1058c727edb845dcb96916f1a9ee6816bbf5b7e6
ttl_hu@163.com <ttl_hu@163.com>
2019-10-11 19:19:45 +0800

    test api

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,8 +2,13 @@ import React,{PureComponent} from 'react';
 
 export default class Home extends PureComponent{
     componentDidMount(){
-        fetch('/api/list').then(res=>{
-            console.log(res)
+        fetch('/api/list',{ 
+            headers: { 'Content-Type': 'application/json;charset=UTF-8'}
+        }).then(res=>{
+            // console.log(res.json())
+            res.json().then(data=>{
+                console.log(data)
+            })
         })
     }
     render() {
